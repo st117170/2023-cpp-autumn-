82e1d61ce8e8f9ef2498cd4f4ee60936bd6eb1bb
veronika sergienko <st117170@student.spbu.ru>
2023-11-21 07:31:16 +0300

    uploading task 9

---
+++ 2023.09.30-homework-3/task9/source9.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+int main(int argc, char *argv[])
+{
+	long int a = 0;
+	long int b = 0;
+
+	std::cin >> a;
+	std::cin >> b;
+
+	while (a != b)
+	{
+		if ((a / 2 > b) && (a % 2 == 0))
+		{
+			a /= 2;
+			std::cout << ":2" << std::endl;
+
+		}
+		else
+		{
+			a -= 1;
+			std::cout << "-1" << std::endl;
+		}
+	}
+	return EXIT_SUCCESS;
+
+}
