e6609f932d81ecccdda53da90a79e158074211d9
veronika sergienko <st117170@student.spbu.ru>
2023-11-21 07:10:51 +0300

    uploading task 6 and fixing some formatting errors (adding a n empty line after "return EXIT_SUCCESS" line)

--- 2023.09.30-homework-3/task1/source1.cpp
+++ 2023.09.30-homework-3/task1/source1.cpp
@@ -21,4 +21,5 @@ int main(int argc, char* argv[])
         }
     }
     return EXIT_SUCCESS;
+
 }
--- 2023.09.30-homework-3/task5/source5.cpp
+++ 2023.09.30-homework-3/task5/source5.cpp
@@ -30,4 +30,5 @@ int main(int argc, char* argv[])
 	}
 	std::cout << ans;
 	return EXIT_SUCCESS;
+
 }
