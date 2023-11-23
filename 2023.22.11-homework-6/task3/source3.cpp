a54f55fee60f5e612a2866ab97a46162ceafd288
veronika sergienko <st117170@student.spbu.ru>
2023-11-24 02:25:41 +0300

    adding task 3

---
+++ 2023.22.11-homework-6/task3/source3.cpp
@@ -0,0 +1,51 @@
+#include<iostream>
+
+void moves_record(int n, int from, int to)
+{
+	printf("%d %d %d\n", n, from, to);
+}
+
+void hanoi(int n, int from, int to) 
+{
+
+	if (n > 0)
+	{
+		int res = 6 - from - to;
+		if (from == to - 1 || (from == 3 && to == 1))
+		{
+			hanoi(n - 1, from, res); 
+			moves_record(n, from, to);
+			hanoi(n - 1, res, to); 
+			
+		}
+		else
+		{
+			hanoi(n - 1, from, to);
+			moves_record(n, from, res);
+			hanoi(n - 1, to, from);
+			moves_record(n, res, to);
+			hanoi(n - 1, from, to);
+			
+		}
+		
+	}
+
+	else
+	{
+		return;
+	}
+}
+
+int main(int argc, char* argv[])
+{
+	int amount = 0;
+	int move_from = 1;
+	int move_to = 3;
+
+	std::cin >> amount;
+
+	hanoi(amount, move_from, move_to);
+	std::cout << std::endl;
+	return EXIT_SUCCESS;
+
+}
