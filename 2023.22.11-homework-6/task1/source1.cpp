1c84be1478697c0d14f6f7d8589dbf0de654be11
veronika sergienko <st117170@student.spbu.ru>
2023-11-23 04:43:03 +0300

    creating branch and folder for homework 6. uploading task 1

---
+++ 2023.22.11-homework-6/task1/source1.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+
+void moves_record(int n, int from, int to)
+{
+	printf("%d %d %d\n", n, from, to);
+}
+
+void hanoi(int n, int from, int to)
+{
+	if (n > 0)
+	{
+		int res = 6 - from - to;
+		hanoi(n - 1, from, res);
+		moves_record(n, from, to);
+		hanoi(n - 1, res, to);
+	}
+	else
+	{
+		return;
+	}
+}
+
+int main(int argc, char* argv[])
+{
+	int amount = 0;
+	int move_from = 1;
+	int move_to = 3;
+
+	std:: cin >> amount;
+
+	hanoi(amount, move_from, move_to);
+	std::cout << std::endl;
+	return EXIT_SUCCESS;
+
+}
